b408af67f4e6120383f1d7a16b93c8116cf61550
dineshtripathi30 <dinesh.tripathi30@gmail.com>
2022-04-08 10:29:52 +0530

    updated file

---
+++ src/main/java/com/example/tapdemo1/Tapdemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.tapdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tapdemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tapdemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tapdemo1/Tapdemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tapdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tapdemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
